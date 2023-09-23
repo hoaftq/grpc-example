81749016f50bd48fdde03abc2f7ea7a9810cb973
hoaftq <hoaftq@gmail.com>
2023-09-23 16:40:27 +0800

    Correcting termination process

--- service1/src/main/java/org/example/Main.java
+++ service1/src/main/java/org/example/Main.java
@@ -5,8 +5,10 @@ import io.grpc.TlsServerCredentials;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+
     public static void main(String[] args) throws IOException, InterruptedException {
         // Path to file service1.pem
         var certChainFilePath = new File(args[0]);
@@ -27,6 +29,15 @@ public class Main {
                 .addService(new CategoryServiceImpl())
                 .build();
         server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                server.shutdown().awaitTermination(500, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
         server.awaitTermination();
     }
 }
--- service2/src/main/java/org/example/Main.java
+++ service2/src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import io.grpc.Grpc;
+import io.grpc.ManagedChannel;
 import io.grpc.TlsChannelCredentials;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
@@ -24,16 +26,23 @@ public class Main {
                 .keyManager(certChainFilePath, privateKeyFilePath) // to use mutual SSL
                 .build();
 
-        var channel = Grpc.newChannelBuilderForAddress("localhost", 4000, chanelCredentials).build();
+        ManagedChannel channel = null;
+        try {
+            channel = Grpc.newChannelBuilderForAddress("localhost", 4000, chanelCredentials).build();
 
-        var productClient = new ProductClient(channel);
-        productClient.useBlockingStub();
-        productClient.useStub();
-        productClient.useFutureStub();
+            var productClient = new ProductClient(channel);
+            productClient.useBlockingStub();
+            productClient.useStub();
+            productClient.useFutureStub();
 
-        var categoryClient = new CategoryClient(channel);
-        categoryClient.executeWithBlockingStub();
-        categoryClient.executeWithStub();
-        categoryClient.executeWithDeadline();
+            var categoryClient = new CategoryClient(channel);
+            categoryClient.executeWithBlockingStub();
+            categoryClient.executeWithStub();
+            categoryClient.executeWithDeadline();
+        } finally {
+            if (channel != null) {
+                channel.shutdownNow().awaitTermination(500, TimeUnit.MILLISECONDS);
+            }
+        }
     }
 }
